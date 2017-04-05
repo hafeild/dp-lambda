8152fa3cee92e10cb4faf2560a78871d526f446c
Henry Feild <hank@feild.org>
2017-04-05 15:26:30 -0400

    Added session support and tests.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,22 +10,22 @@ class ActiveSupport::TestCase
   include ApplicationHelper
 
   # Returns true if a test user is logged in.
-  # def is_logged_in?
-  #   !session[:user_id].nil?
-  # end
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 
-  # # Logs in a test user.
-  # def log_in_as(user, options = {})
-  #   password    = options[:password]    || 'password'
-  #   remember_me = options[:remember_me] || '1'
-  #   if integration_test?
-  #     post login_path, session: { username:    user.username,
-  #                                 password:    password,
-  #                                 remember_me: remember_me }
-  #   else
-  #     session[:user_id] = user.id
-  #   end
-  # end
+  # Logs in a test user.
+  def log_in_as(user, options = {})
+    password    = options[:password]    || 'password'
+    remember_me = options[:remember_me] || '1'
+    if integration_test?
+      post login_path, session: { username:    user.username,
+                                  password:    password,
+                                  remember_me: remember_me }
+    else
+      session[:user_id] = user.id
+    end
+  end
 
 
   private
