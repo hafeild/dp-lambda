312abde710afcbc70ba5893a4279f18687aa8e4e
Henry Feild <hfeild@endicott.edu>
2017-05-31 11:57:48 -0400

    Updated the way user html content is displayed.
    Updated the way examples are shown.

--- app/assets/javascripts/00-general.js
+++ app/assets/javascripts/00-general.js
@@ -1,10 +1,32 @@
+/**
+ * Consists of scripts for all pages.
+ *
+ * Created by hfeild on 30-May-2017.
+ */
 
+
+/**
+ * Finds all iframe tags with the class 'embed-html', then creates a document
+ * in the iframe and sticks whatever is in the 'data-html' attribute for the
+ * iframe tag. The value of 'data-html' is assumed to be URL encoded.
+ */
 var embed_html = function(){
+  var computed_style = window.getComputedStyle(document.body);
+  
   $('iframe.embed-html').each(function(i, elm){
     this.contentWindow.document.open();
     this.contentWindow.document.write(decodeURIComponent($(this).data('html')));
     this.contentWindow.document.close();
-    this.style.height = this.contentWindow.document.height;
+
+    // Set fonts.
+    this.contentWindow.document.body.style.fontFamily = 
+      computed_style.fontFamily;
+    this.contentWindow.document.body.style.fontSize =
+      computed_style.fontSize;
+
+    // Set the height of the frame to match the document.
+    $(this).height(this.contentWindow.document.body.scrollHeight);
+
   });
 
 }
