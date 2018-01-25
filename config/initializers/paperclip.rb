cf524109aa74142dd5308864960ce569d894744c
Henry Feild <hfeild@endicott.edu>
2018-01-25 09:48:17 -0500

    Added paperclip extension->mimetype map.
    This way cpp and py files can be uploaded. More extensions probably need to be
    mapped. See config/initializers/paperclip.rb for the current mappings.

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,14 @@
+## Supported file types for Paperclip.
+Paperclip.options[:content_type_mappings] = {
+  cpp: %w(text/x-c++ text/x-c text/plain),
+  py: %w(text/x-c++ text/x-c text/plain),
+  h: %w(text/x-c++ text/x-c text/plain),
+  rb: %w(text/x-c++ text/x-c text/plain),
+  lua: %w(text/x-c++ text/x-c text/plain),
+  csv: %w(text/x-c++ text/x-c text/plain),
+  tsv: %w(text/x-c++ text/x-c text/plain),
+  dat: %w(text/x-c++ text/x-c text/plain),
+  pl: %w(text/x-c++ text/x-c text/plain),
+  java: %w(text/x-c++ text/x-c text/plain),
+  json: %w(text/x-c++ text/x-c text/plain)
+}
