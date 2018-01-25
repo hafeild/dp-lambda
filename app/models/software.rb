0c5482b7ec5d0b76978cb1966be68e8918619380
Henry Feild <hank@feild.org>
2018-01-25 06:25:20 -0500

    Added attachment upload for software pages.

--- app/models/software.rb
+++ app/models/software.rb
@@ -19,6 +19,7 @@ class Software < ApplicationRecord
   # has_and_belongs_to_many :software
   # has_and_belongs_to_many :datasets
   # has_and_belongs_to_many :analyses
+  has_and_belongs_to_many :attachments
 
   ## Ensure the presence of required fields. 
   validates :name, presence: true, length: {maximum: 200}, 
