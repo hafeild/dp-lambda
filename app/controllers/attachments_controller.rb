64c25914b1984a979dc1ae1ddda893ba85ea54bb
Henry Feild <hank@feild.org>
2018-05-14 06:29:40 -0400

    Playing around with file renaming still.

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -50,7 +50,10 @@ class AttachmentsController < ApplicationController
         end
 
         if p.has_key?(:file_attachment_file_name)
-          attachment.file_attachment.update!(file_name: p.require(:file_attachment_file_name))
+          #attachment.file_attachment.file_name = p.require(:file_attachment_file_name)
+          attachment.file_attachment.instance_write(:file_name, p.require(:file_attachment_file_name))
+          attachment.save!
+          #attachment.file_attachment.update!(file_name: p.require(:file_attachment_file_name))
         end
 
         if p.has_key?(:description)
