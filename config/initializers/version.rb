1eb05c6abcf9601d214aca97f03c3172c76654d2
Hank Feild <hank@feild.org>
2018-08-17 09:03:38 -0400

    Fixed minor version misnumbering.

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -15,8 +15,8 @@
 isRelease = true
 yearVersion   = "18" ## Year of release.
 monthVersion  = "08" ## Month of release.
-numberVersion = "01" ## Number of release within Year-Month
-hotFixNo      = "01" ## Hot fix no. for release.
+numberVersion = "00" ## Number of release within Year-Month
+hotFixNo      = "00" ## Hot fix no. for release.
 
 VERSION = [
   yearVersion, 
