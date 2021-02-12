13728f610fc9cc326a0e551906c6d683d267a631
Hank Feild <hank@feild.org>
2021-02-12 12:29:22 -0500

    Added docker files.

---
+++ docker/scripts/run-dev-container.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#####
+# Fires up the development container; use ctrl-c to drop to the command line,
+# and then exit to stop and remove the container. The alice repo folder
+# on the host is mounted as a volume, so all files (logs, sqlite database, etc.)
+# are persistent.
+#####
+
+## Grab the development host port from config/application.yml (DEV_HOST_PORT).
+hostPort=$(grep DEV_HOST_PORT: config/application.yml | perl -pe 's/(^.*: )|\s|"//g')
+
+docker run -ti --rm -p $hostPort:3000 -v "$PWD:/usr/src/app" alice-dev
---
+++ docker/scripts/setup-compose-file.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+## Creates/overwrites the file docker/Compose.prod.yml, filled with the host 
+## port specified in config/application.yaml (PROD_HOST_PORT).
+
+hostPort=$(grep PROD_HOST_PORT: config/application.yml | perl -pe 's/(^.*: )|\s|"//g')
+
+cat > docker/Compose.prod.yml << EOF
+services:
+  alice_db:
+    image: postgres
+    volumes:
+      - ../prod-db:/var/lib/postgresql/data
+    environment:
+      POSTGRES_PASSWORD: password
+  web:
+    build: 
+      context: ../
+      dockerfile: docker/Docker.prod
+    command: ash docker/scripts/prod-start.sh
+    volumes:
+      - ..:/usr/src/app
+    ports:
+      - "$hostPort:5000"
+    depends_on:
+      - alice_db
+EOF
